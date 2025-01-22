c1abb44d3c9d93b3622a1bc9695d36221e69c575
Rishabh Sharma <rishabhsharma8734@gmail.com>
2025-01-22 09:51:49 +0530

    second commit

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,11 +1,11 @@
-
+import Navbar  from "./components/Navbar/Navbar";
 
 function App() {
   
 
   return (
     <div className="bg-[#171d32] h-auto w-full overflow-hidden">
-
+     <Navbar/>
     </div>
   )
 
